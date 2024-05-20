d31d61af27d799cfbe77e700773b39fb78632d98
Christoph Tack <prog.send@gmail.com>
2024-05-20 15:47:28 +0200

    External interrupt works

--- hedwig-firmware/src/main.cpp
+++ hedwig-firmware/src/main.cpp
@@ -1,19 +1,23 @@
-/*
- * ATtiny402LowPowerRunning.c
- * These settings (F_CPU 32000UL, 0 prescaler, all GPIOs apart from LED pin set to pullup inc UPDI, force OSC20M off)
- * Result in a processor current draw of 16uA!
- * Created: 05/05/2019 20:46:56
- * Author : Simon, https://hackaday.io/project/165439/logs
- */ 
+/**
+ * @file main.cpp
+ * @author your name (you@domain.com)
+ * @brief Harry Potter Hedwig Lamp Controller
+ * @version 0.1
+ * @date 2024-05-20
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
 
 #ifndef F_CPU
 #define F_CPU 32000UL // 32 kHz clock speed / 0 prescaler divisor
 #endif
 
-#include <util/delay.h>	// needed for our delay
+#include <util/delay.h> // needed for our delay
 #include <avr/io.h>
+#include <avr/interrupt.h>
 
-int main (void)
+int main(void)
 {
 
 	/* Set the Main clock to internal 32kHz oscillator*/
@@ -23,30 +27,37 @@ int main (void)
 	/* ensure 20MHz isn't forced on*/
 	_PROTECTED_WRITE(CLKCTRL.OSC20MCTRLA, CLKCTRL.OSC20MCTRLA & ~CLKCTRL_RUNSTDBY_bm);
 
-	/* Configure Port A, Pin 6 as an output (remember to connect LED to PB6 and use a resistor in series to GND)*/
-	PORTA.DIRSET = PIN6_bm;
-	
+	PORTA.DIRSET = PIN6_bm | PIN3_bm;
+
 	/* Set all pins except the LED pin to pullups*/
 	PORTA.PIN0CTRL = PORT_PULLUPEN_bm;
 	PORTA.PIN1CTRL = PORT_PULLUPEN_bm;
 	PORTA.PIN2CTRL = PORT_PULLUPEN_bm;
-	PORTA.PIN3CTRL = PORT_PULLUPEN_bm;
-	//PORTA.PIN6CTRL = PORT_PULLUPEN_bm; // LED pin
-	PORTA.PIN7CTRL = PORT_PULLUPEN_bm;
-	
-	
+
+	// Pin 3 is the input pin, connected to the switch
+	PORTA.PIN7CTRL = PORT_PULLUPEN_bm | PORT_ISC_BOTHEDGES_gc;
+
+	sei();
+
 	while (1)
 	{
-		// PAUSE 1000 milliseconds
 		_delay_ms(1000);
-		
-		// LED off 
+
 		PORTA.OUTCLR = PIN6_bm;
-		
-		// PAUSE 2000 milliseconds
+		PORTA.OUTSET = PIN3_bm;
+
 		_delay_ms(1000);
-		
-		// LED on
+
 		PORTA.OUTSET = PIN6_bm;
+		PORTA.OUTCLR = PIN3_bm;
 	}
+}
+
+ISR(PORTA_PORT_vect)
+{
+	uint8_t flags = VPORTA.INTFLAGS; // read the interrupt flags
+	// writing "1" to the interruptflag bit will clear it.
+	VPORTA_INTFLAGS = flags; // clear interrupt flags
+	PORTA.OUTSET = PIN6_bm;
+	PORTA.OUTSET = PIN3_bm;
 }
